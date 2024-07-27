a4f078de717da37d2930f80940f6b278a2d665ab
Ismail Salah <ismailsalah@Ismails-MacBook-Air.local>
2024-07-27 20:04:51 +0300

    Delete About Us

--- app/[locale]/(marketing)/page.tsx
+++ app/[locale]/(marketing)/page.tsx
@@ -1,14 +1,12 @@
 import { Fragment } from "react";
 import type { NextPage } from "next";
 import { MainBanner } from "@/app/components/widgets/main-banner";
-import { AboutUs } from "@/app/components/widgets/about-us";
 const Home: NextPage<{ params: { locale: any } }> = async ({
 	params: { locale },
 }) => {
 	return (
 		<Fragment>
 			<MainBanner locale={locale} />
-			<AboutUs />
 		</Fragment>
 	);
 };
--- app/constants/castImages.ts
+++
@@ -1,4 +0,0 @@
-import cast_1 from '@/public/static/desktop/cast-1.png';
-
-
-export const CAST_IMAGES = [ cast_1, cast_1,cast_1,cast_1, cast_1,cast_1 ];
