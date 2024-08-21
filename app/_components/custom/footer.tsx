8f46e9f84d256e609adca26c21fdd27e7cd5acaa
Ismail Salah <ismailsalah@Ismails-MacBook-Air.local>
2024-08-21 03:40:32 +0300

    Footer

--- app/_components/custom/footer.tsx
+++ app/_components/custom/footer.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import Link from "next/link";
 import initTranslations from "@/app/_config/i18n";
 import { FOOTER_BACKGROUND } from "@/app/_constants/footer";
 const i18Namespaces = ["common"];
@@ -7,7 +8,7 @@ export const Footer: FC<{ locale: any }> = async ({ locale }) => {
 	const { t } = await initTranslations(locale, i18Namespaces);
 	return (
 		<footer
-			className="h-[500px] mt-16 bg-center bg-cover bg-no-repeat text-white"
+			className="h-[500px] mt-16 bg-center bg-cover bg-no-repeat text-white relative"
 			style={{ backgroundImage: `url(${FOOTER_BACKGROUND.src})` }}
 		>
 			<section className="lg:mx-24 space-y-12">
@@ -16,6 +17,44 @@ export const Footer: FC<{ locale: any }> = async ({ locale }) => {
 					{t("common:footer.about_us.description")}
 				</p>
 			</section>
+			<section className="flex gap-12 lg:mx-24 mt-16 text-slate-300">
+				<ul className="flex flex-col gap-6">
+					<li>
+						<Link
+							target="_blank"
+							href="https://qualityteamiq.ladesk.com"
+						>
+							{t("common:footer.links.support")}
+						</Link>
+					</li>
+					<li>
+						<Link href="/about-us#contact">
+							{t("common:footer.links.contact")}
+						</Link>
+					</li>
+				</ul>
+				<ul className="flex flex-col gap-6">
+					<li>
+						<Link
+							target="_blank"
+							href="https://www.facebook.com/profile.php?id=61551994790268"
+						>
+							{t("common:footer.links.facebook")}
+						</Link>
+					</li>
+					<li>
+						<Link
+							target="_blank"
+							href="https://www.instagram.com/qualityteam.iq/"
+						>
+							{t("common:footer.links.instagram")}
+						</Link>
+					</li>
+				</ul>
+			</section>
+			<span className="lg:mx-24 absolute bottom-44">
+				{t("common:footer.attr")}
+			</span>
 		</footer>
 	);
 };
