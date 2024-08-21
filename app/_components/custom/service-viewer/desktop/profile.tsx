245ca773b76acf8ef452a5660170427dd47a7382
Ismail Salah <ismailsalah@Ismails-MacBook-Air.local>
2024-08-21 16:10:45 +0300

    Fix: Images

--- app/_components/custom/service-viewer/desktop/profile.tsx
+++ app/_components/custom/service-viewer/desktop/profile.tsx
@@ -5,7 +5,13 @@ import Image from "next/image";
 export const Profile: FC<profileType> = ({ image }) => {
 	return (
 		<div className="md:h-96 md:w-[380px]">
-			<Image src={image} height={500} width={500} alt="Camera man" />
+			<Image
+				priority
+				src={image}
+				height={500}
+				width={500}
+				alt="Camera man"
+			/>
 		</div>
 	);
 };
