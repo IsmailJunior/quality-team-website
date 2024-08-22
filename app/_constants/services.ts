2076d0119e8db3a3d058d9c8481787cff10b01e5
Ismail Salah <ismailsalah@Ismails-MacBook-Air.local>
2024-08-22 05:07:38 +0300

    payload

--- app/_constants/services.ts
+++ app/_constants/services.ts
@@ -1,23 +1,15 @@
-import GRAPHIC_DESIGN from '@/public/static/desktop/graphic-design.png';
-import CONTENT_CREATION from '@/public/static/desktop/content-creation.png';
-import VIDEO_EDITING from '@/public/static/desktop/video-editing.png';
-import MARKETING from '@/public/static/desktop/marketing.png';
 import CAMERA_MAN from '@/public/static/desktop/camera_man.png';
 export const SERVICES = {
 	graphic_design: {
-		thumnail: GRAPHIC_DESIGN,
 		picture: CAMERA_MAN
 	},
 	content_creation: {
-		thumnail: CONTENT_CREATION,
 		picture: CAMERA_MAN
 	},
 	video_editing: {
-		thumnail: VIDEO_EDITING,
 		picture: CAMERA_MAN
 	},
 	marketing: {
-		thumnail: MARKETING,
 		picture: CAMERA_MAN
 	}
 };
